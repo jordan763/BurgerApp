867f223f70be93710042a414daac920c8b9370c5
Jordan Vang <jordanvang8@gmail.com>
2020-11-02 14:46:09 -0600

    update

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,22 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
 
+var mysql = require('mysql');
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "1qaz2wsx!QAZ@WSX",
-    database: "cat_db"
-  });
-}
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: '127.0.0.1',
+        user: 'root',
+        password: '1qaz2wsx!QAZ@WSX',
+        database: 'cat_db'
+    })
+};
+
+var mysql = require('mysql');
 
 // Make connection.
 connection.connect() 
