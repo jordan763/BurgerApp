7a7b447f9f912e5a3a4496bda7c2787192926a01
Jordan Vang <jordanvang8@gmail.com>
2020-11-01 21:47:39 -0600

    fixed submit button

--- public/assets/js/cats.js
+++ public/assets/js/cats.js
@@ -21,6 +21,24 @@ $(function() {
     );
   });
 
+  $(".delplan").on("click", function(event) {
+    // Get the ID from the button.
+    // This is shorthand for $(this).attr("data-planid")
+    var id = $(this).data("planid");
+  
+    // Send the DELETE request.
+    $.ajax("/api/cats/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted id ", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+  
+
 
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
